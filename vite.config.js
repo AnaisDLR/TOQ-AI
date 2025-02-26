e2827ee206d244ff29455dc7fd2d02523157dadf
AnaisDLR <95475174+AnaisDLR@users.noreply.github.com>
2025-02-26 16:14:41 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
+import { loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite"
-
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
