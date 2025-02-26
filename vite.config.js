5657cbc838e27d3b2c22ea4e042c8eb988d3dc4e
AnaisDLR <95475174+AnaisDLR@users.noreply.github.com>
2025-02-26 15:44:31 +0100

    Add GitHub Actions workflow

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,23 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite"
 
 
-export default defineConfig(async () => ({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  base: '/TOQ-AI/',
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+    ],
+    base: '/TOQ-AI/',
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: undefined
+        }
       }
+    },
+    define: {
+      'process.env.VITE_API_KEY': JSON.stringify(env.VITE_REACT_APP_API_KEY)
     }
   }
-}))
+})
